98c713e3e468c1b3f61e05c2172c80486ea5ce2f
Indradhanush Gupta <dhanush@instamojo.com>
2015-02-02 00:40:36 +0530

    Code refactor, fix bugs.

---
+++ settings.py
@@ -0,0 +1,28 @@
+"""
+Project settings for birthday-thanks
+"""
+
+# Bot settings
+ACCESS_TOKEN = ''
+
+# Set this to your full facebook name.
+NAME = 'Indradhanush Gupta'
+BASE_URL = 'https://graph.facebook.com'
+
+# Time range in seconds since epoch.
+START_TIME = 1422729000
+END_TIME = 1422815400
+
+REPLIES = [
+    'Thanks!! :)',
+    'Thank You!!  :)',
+    'Thanks a lot!! :)',
+    'Thanks a ton!! :)',
+    'Thanks for your wishes!! :)',
+    'Thannkk youu! :)',
+    'Thankksss a lottt! :)',
+    'Thankksss a tonnn for the wishes! :)',
+]
+
+
+
--- thanks_bot.py
+++ thanks_bot.py
@@ -1,87 +1,119 @@
-import simplejson
+"""
+Birthday thanks bot.
+"""
+
+# System imports
+import sys
 import random
-import requests
 import re
 
+# Third party imports
+import simplejson
+import requests
 
-# Bot settings
-ACCESS_TOKEN = ""
-
-# Set this to your full facebook name.
-NAME = "Indradhanush Gupta"
-BASE_URL = "https://graph.facebook.com"
-
-# Time in seconds since epoch.
-START_TIME = 1391193000
-END_TIME = 1391279400
-
-
-THANKYOU = [
-    "Thanks!! :)",
-    "Thank You!!  :)",
-    "Thanks a lot!! :)",
-    "Thanks a ton!! :)",
-    "Thanks for your wishes!! :)"
-]
+# Local imports
+import settings
 
 regex = re.compile(".*h+a*p+i*y*e*(.*b([irthday]|['.]|(u*(d+(a+e+|a+y+|y+))))|(.*return(s|'s)))")
 
 
-def get_wishes(START_TIME, END_TIME):
+def get_wishes(start_time, end_time):
+    """
+    Fetches facebook wall posts between a given time range.
+
+    :param start_time: Lower range of seconds since epoch.
+    :type start_time: int
+    :param _time: Upper range of seconds since epoch.
+    :type _time: int
+
+    :return: List of posts
+    :type: list
+    """
+    sys.stdout.write('Fetching your wishes...')
+    sys.stdout.flush()
     payload = {
-        "q" : "SELECT actor_id, post_id, message FROM stream WHERE source_id = me() AND created_time > %d AND created_time < %d limit 200;" % (START_TIME, END_TIME),
-        "access_token" : ACCESS_TOKEN
+        "q" : "SELECT actor_id, post_id, message FROM stream WHERE source_id = me() AND created_time > %d AND created_time < %d limit 200;" % (start_time, end_time),
+        "access_token" : settings.ACCESS_TOKEN
     }
-    url = "%s/fql" % (BASE_URL)
+    url = "%s/fql" % (settings.BASE_URL)
     response = requests.get(url, params=payload)
     data_set = simplejson.loads(response.text)["data"]
-    print len(data_set)
+    sys.stdout.write('OK\n')
+    sys.stdout.flush()
+
     return data_set
 
 
-def post_thanks(data_set):
-    for data in data_set:
-        wish = data["message"]
-        if regex.match(wish):
-            pass
-        else:
-            print "Regex match failed for message: %s" % (wish)
-            override_regex = raw_input("Post thanks? (Y/N):")
-            if override_regex == "y" or override_regex == "Y":
-                pass
-            else:
-                continue
-            
-        post_id = data["post_id"]
-        message = THANKYOU[random.randint(0, 4)]
-        params = {"access_token": ACCESS_TOKEN}
-        print wish, post_id
-        comment_url = "%s/%s/comments" % (BASE_URL, post_id)        
-        
-        response = requests.get(comment_url, params=params)
-        comments = simplejson.loads(response.text)["data"]
-        reply_flag = False
-        for comment in comments:
-            if comment["from"]["name"] == NAME: #Already Commented.
-                reply_flag = True
-                break
-        if reply_flag:
-            print "Commented!"
-            continue
-        else:
-            #Like the post.
-            like_url = "%s/%s/likes" % (BASE_URL, post_id)
-            params = {"access_token": ACCESS_TOKEN}
-            response = requests.post(like_url, params=params)
-            print response.text
+def post_thanks(data_set, match_regex=True):
+    """
+    Likes and comments on posts.
 
-            # Comment thanks on post.
-            params["message"] = message
-            response = requests.post(comment_url, data=params)
-            print response.text
+    :param data_set: List of facebook posts.
+    :type data_set: list
+    :param match_regex: Indicate whether post message is to be matched
+                        with regex or not.
+    :type match_regex: bool
+
+    :return: List of posts that failed regex match.
+    :type: list
+    """
+    failed_matches = []
+
+    sys.stdout.write('Posting...')
+    sys.stdout.flush()
+    for data in data_set:
+        wish = data["message"].lower().strip()
+        if match_regex and regex.match(wish) is None:
+            failed_matches.append(data)
+        else:
+            post_id = data["post_id"]
+            message = settings.REPLIES[random.randint(0, 4)]
+            params = {
+                "access_token": settings.ACCESS_TOKEN
+            }
+            comment_url = "%s/%s/comments" % (settings.BASE_URL, post_id)
+
+            response = requests.get(comment_url, params=params)
+            comments = simplejson.loads(response.text)["data"]
+            already_replied = False
+
+            for comment in comments:
+                if comment["from"]["name"] == settings.NAME: # Already Commented.
+                    already_replied = True
+                    break
+
+            if not already_replied:
+                #Like the post.
+                like_url = "%s/%s/likes" % (settings.BASE_URL, post_id)
+                response = requests.post(like_url, data=params)
+                sys.stdout.write('.')
+                sys.stdout.flush()
+
+                # Comment thanks on post.
+                params["message"] = message
+                response = requests.post(comment_url, data=params)
+                sys.stdout.write('.')
+                sys.stdout.flush()
+
+    sys.stdout.write('OK\n')
+
+    return failed_matches
 
 
 if __name__ == "__main__":
-    data_set = get_wishes(START_TIME, END_TIME)
-    post_thanks(data_set)
+    data_set = get_wishes(settings.START_TIME, settings.END_TIME)
+    failed_matches = post_thanks(data_set)
+    if failed_matches:
+        check_failed_matches = raw_input(
+            'Regex failed for some posts. Wanna check? (Y/N):')
 
+        if check_failed_matches.lower().strip() == 'y':
+            force_post = []
+            for msg in failed_matches:
+                print 'Failed message: {msg}'.format(msg=msg['message'])
+                override_regex = raw_input('Post thanks? (Y/N):')
+                if override_regex.lower().strip() == 'y':
+                    force_post.append(msg)
+
+            if force_post:
+                post_thanks(force_post, match_regex=False)
